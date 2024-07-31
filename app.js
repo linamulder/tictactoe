f8e790dc3ae8e15361351d1fdd0b758c49d319db
Linavo <linaqvo@gmail.com>
2024-07-31 13:29:34 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//first I need to create a Gameboard object to store the gameboard as an array
+const gameBoard = {
+  lettersArray: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+  playerOne: "",
+  playerTwo: "",
+  choice: "none",
+  didIRestart: false,
+};
+
+console.log("testing");
